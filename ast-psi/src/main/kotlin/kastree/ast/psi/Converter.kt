adea26e136cf43f05dba92dde64945784a71a021
Chad Retz <chad.retz@gmail.com>
2018-07-14 14:33:48 -0500

    More work on converter

--- ast-psi/src/main/kotlin/kastree/ast/psi/Converter.kt
+++ ast-psi/src/main/kotlin/kastree/ast/psi/Converter.kt
@@ -29,6 +29,9 @@ open class Converter {
     )
 
     fun convertDecl(v: KtDeclaration): Node.Decl = when (v) {
+        is KtAnonymousInitializer -> convertInit(v)
+        is KtClassOrObject -> convertStructured(v)
+        is KtFunction -> convertFunc(v)
         else -> TODO()
     }
 
@@ -39,24 +42,71 @@ open class Converter {
         decls = v.declarations.map(::convertDecl)
     )
 
+    fun convertFunc(v: KtFunction): Node.Decl.Func = TODO()
+
     fun convertImport(v: KtImportDirective) = Node.Import(
         names = v.importedFqName?.pathSegments()?.map { it.asString() } ?: error("Missing import path"),
         wildcard = v.isAllUnder,
         alias = v.aliasName
     )
 
-    fun convertModifiers(v: KtModifierListOwnerStub<*>): List<Node.Modifier> = TODO()
+    fun convertInit(v: KtAnonymousInitializer) = Node.Decl.Init(
+        stmts = (v.body as? KtBlockExpression)?.statements?.map(::convertStmt) ?: emptyList()
+    )
+
+    fun convertModifiers(v: KtModifierListOwnerStub<*>): List<Node.Modifier> {
+        // We want to preserve the order
+        return v.children.mapNotNull {
+            TODO("Work with $it")
+        }
+    }
 
     fun convertPackage(v: KtPackageDirective) = Node.Package(
         mods = convertModifiers(v),
         names = v.packageNames.map { it.getReferencedName() }
     )
 
+    fun convertParent(v: KtSuperTypeListEntry): Node.Decl.Structured.Parent = TODO()
+
+    fun convertPrimaryConstructor(v: KtPrimaryConstructor): Node.Decl.Structured.PrimaryConstructor = TODO()
+
+    fun convertStmt(v: KtExpression): Node.Stmt = TODO()
+
+    fun convertStructured(v: KtClassOrObject) = Node.Decl.Structured(
+        mods = convertModifiers(v),
+        form = when (v) {
+            is KtClass ->
+                if (v.isEnum()) Node.Decl.Structured.Form.ENUM_CLASS
+                else if (v.isInterface()) Node.Decl.Structured.Form.INTERFACE
+                else Node.Decl.Structured.Form.CLASS
+            is KtObjectDeclaration ->
+                if (v.isCompanion()) Node.Decl.Structured.Form.COMPANION_OBJECT
+                else Node.Decl.Structured.Form.OBJECT
+            else -> error("Unknown type of $v")
+        },
+        name = v.name ?: error("Missing name"),
+        typeParams = v.typeParameters.map(::convertTypeParam),
+        primaryConstructor = v.primaryConstructor?.let(::convertPrimaryConstructor),
+        parentAnns = emptyList<Node.Modifier.AnnotationSet>().also { TODO() },
+        parents = v.superTypeListEntries.map(::convertParent),
+        typeConstraints = v.typeConstraints.map(::convertTypeConstraint),
+        members = v.declarations.map(::convertDecl)
+    )
+
     fun convertType(v: KtTypeProjection): Node.Type = TODO()
+
+    fun convertTypeConstraint(v: KtTypeConstraint): Node.TypeConstraint = TODO()
+
+    fun convertTypeParam(v: KtTypeParameter): Node.TypeParam = TODO()
 
     fun convertValueArg(v: ValueArgument): Node.ValueArg = TODO()
 
     companion object : Converter() {
+        // TODO: Use KtTokens.MODIFIER_KEYWORDS_ARRAY
+//        val modifierTokens = Node.Modifier.Keyword.values().map {
+//            KtModifierKeywordToken.keywordModifier(it.name.toLowerCase())
+//        }
+
         val KtTypeReference.names get() = (typeElement as? KtUserType)?.names ?: emptyList()
         val KtUserType.names get(): List<String> =
             referencedName?.let { (qualifier?.names ?: emptyList()) + it } ?: emptyList()
