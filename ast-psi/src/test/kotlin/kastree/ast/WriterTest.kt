22e035b8efdc2998be06c9e3279531003129d984
Chad Retz <chad.retz@gmail.com>
2019-03-03 12:20:55 -0600

    Add short test cases for #20, #21, and #22

--- ast-psi/src/test/kotlin/kastree/ast/WriterTest.kt
+++ ast-psi/src/test/kotlin/kastree/ast/WriterTest.kt
@@ -13,6 +13,21 @@ class WriterTest {
         assertParseAndWriteExact("const val c = `___`")
     }
 
+    @Test
+    fun testTypeParameterModifiers() {
+        assertParseAndWriteExact("fun delete(p: Array<out String>?) {}")
+    }
+
+    @Test
+    fun testSimpleCharacterEscaping() {
+        assertParseAndWriteExact("""val x = "input\b\n\t\r\'\"\\\${'$'}"""")
+    }
+
+    @Test
+    fun testSuperclassPrimaryConstructor() {
+        assertParseAndWriteExact("private object SubnetSorter : DefaultSorter<Subnet>()")
+    }
+
     fun assertParseAndWriteExact(code: String) {
         assertEquals(code.trim(), Writer.write(Parser.parseFile(code)).trim())
     }
